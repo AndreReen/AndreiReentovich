22312b41d569a75c999d5cae6a8a314b5c7a9a19
AndreReen <43380198+AndreReen@users.noreply.github.com>
2021-05-04 11:42:29 +0300

    Homework2 with updated req branch commit

--- src/test/java/ru/training/at/hw2/ex1/Exercise1.java
+++ src/test/java/ru/training/at/hw2/ex1/Exercise1.java
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.List;
+
 
 public class Exercise1 {
 
@@ -17,6 +19,7 @@ public class Exercise1 {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
         webDriver = new ChromeDriver();
         webDriver.manage().window().maximize();
+        SoftAssert assertS = new SoftAssert();
 
         // 1. Open test site by URl
         webDriver.navigate().to("https://jdi-testing.github.io/jdi-light/index.html");
@@ -24,7 +27,7 @@ public class Exercise1 {
         // 2. Assert browser title
         String actualTitle = webDriver.getTitle();
         String expectedTitle = "Home Page";
-        Assert.assertEquals(actualTitle, expectedTitle);
+        assertS.assertEquals(actualTitle, expectedTitle);
 
         // 3. Perform login
         WebElement webElement = webDriver.findElement(By.id("user-icon"));
@@ -35,74 +38,62 @@ public class Exercise1 {
         webElement.click();
 
         // 4. Assert User name in the right-top side of screen that user is logged in
-        webElement = webDriver.findElement(By.id("user-name"));
-        String actualUser = webElement.getText();
+        String actualUser = webDriver.findElement(By.id("user-name")).getText();
         String expectedUser = "ROMAN IOVLEV";
-        Assert.assertEquals(actualUser, expectedUser);
+        assertS.assertEquals(actualUser, expectedUser);
 
-        // 5. Assert browser title
-        actualTitle = webDriver.getTitle();
-        expectedTitle = "Home Page";
-        Assert.assertEquals(expectedTitle, actualTitle);
+        // 5. Assert that there are 4 items on the header section are displayed and they have proper texts
+        assertS.assertTrue(webDriver.findElement(By.xpath("//header//li[1]")).isDisplayed());
+        assertS.assertEquals(webDriver.findElement(By.xpath("//header//li[1]")).getText(), "HOME");
+        assertS.assertTrue(webDriver.findElement(By.xpath("//header//li[2]")).isDisplayed());
+        assertS.assertEquals(webDriver.findElement(By.xpath("//header//li[2]")).getText(), "CONTACT FORM");
+        assertS.assertTrue(webDriver.findElement(By.xpath("//header//li[3]")).isDisplayed());
+        assertS.assertEquals(webDriver.findElement(By.xpath("//header//li[3]")).getText(), "SERVICE");
+        assertS.assertTrue(webDriver.findElement(By.xpath("//header//nav/ul/li[4]")).isDisplayed());
+        assertS.assertEquals(webDriver.findElement(By.xpath("//header//nav/ul/li[4]")).getText(), "METALS & COLORS");
 
-        // 6. Assert header section
-        Assert.assertTrue(webDriver.findElement(By.xpath("//header//li[1]")).isDisplayed());
-        Assert.assertEquals("HOME", webDriver.findElement(By.xpath("//header//li[1]")).getText());
-        Assert.assertTrue(webDriver.findElement(By.xpath("//header//li[2]")).isDisplayed());
-        Assert.assertEquals("CONTACT FORM", webDriver.findElement(By.xpath("//header//li[2]")).getText());
-        Assert.assertTrue(webDriver.findElement(By.xpath("//header//li[3]")).isDisplayed());
-        Assert.assertEquals("SERVICE", webDriver.findElement(By.xpath("//header//li[3]")).getText());
-        Assert.assertTrue(webDriver.findElement(By.xpath("//header//nav/ul/li[4]")).isDisplayed());
-        Assert.assertEquals("METALS & COLORS", webDriver.findElement(By.xpath("//header//nav/ul/li[4]")).getText());
+        //6. Assert that there are 4 images on the Index Page and they are displayed
+        assertS.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-practise']")).isDisplayed());
+        assertS.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-custom']")).isDisplayed());
+        assertS.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-multi']")).isDisplayed());
+        assertS.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-base']")).isDisplayed());
 
-        //7. Assert 4 images on index
-        Assert.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-practise']")).isDisplayed());
-        Assert.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-custom']")).isDisplayed());
-        Assert.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-multi']")).isDisplayed());
-        Assert.assertTrue(webDriver.findElement(By.xpath("//span[@class='icons-benefit icon-base']")).isDisplayed());
-
-        //8.Assert that there are 4 texts on the Index Page under icons and they have proper text
+        //7. Assert that there are 4 texts on the Index Page under icons and they have proper text
         String benefitPractise = "To include good practices\n" + "and ideas from successful\n" + "EPAM project";
         String benefitCustom = "To be flexible and\n" + "customizable";
         String benefitMulti = "To be multiplatform";
         String benefitBase = "Already have good base\n" + "(about 20 internal and\n" + "some external projects),\n" + "wish to get more…";
 
-        Assert.assertEquals(webDriver.findElement(By.xpath("//span[text()='To include good practices']")).getText(), benefitPractise);
-        Assert.assertEquals(webDriver.findElement(By.xpath("//span[text()='To be flexible and']")).getText(), benefitCustom);
-        Assert.assertEquals(webDriver.findElement(By.xpath("//span[text()='To be multiplatform ']")).getText(), benefitMulti);
-        Assert.assertEquals(webDriver.findElement(By.xpath("//span[text()='Already have good base']")).getText(), benefitBase);
+        assertS.assertEquals(webDriver.findElement(By.xpath("//span[text()='To include good practices']")).getText(), benefitPractise);
+        assertS.assertEquals(webDriver.findElement(By.xpath("//span[text()='To be flexible and']")).getText(), benefitCustom);
+        assertS.assertEquals(webDriver.findElement(By.xpath("//span[text()='To be multiplatform ']")).getText(), benefitMulti);
+        assertS.assertEquals(webDriver.findElement(By.xpath("//span[text()='Already have good base']")).getText(), benefitBase);
 
 
-        //9.Assert a text of the main headers
-        SoftAssert assertS = new SoftAssert();
-        Assert.assertEquals("EPAM FRAMEWORK WISHES…", webDriver.findElement(By.name("main-title")).getText());
-        assertS.assertEquals("LOREM IPSUM...", webDriver.findElement(By.name("jdi-text")).getText());
+        //8. Assert that there is the iframe with “Frame Button” exist
+        assertS.assertTrue(webDriver.findElement(By.id("frame")).isDisplayed());
 
-        //10.Assert that there is the iframe in the center of page
-        Assert.assertTrue(webDriver.getPageSource().contains("iframe"));
+        //9. Switch to the iframe and check that there is “Frame Button” in the iframe
+        webDriver.switchTo().frame("frame");
+        assertS.assertTrue(webDriver.findElement(By.id("frame-button")).isDisplayed());
 
-        //11.Switch to the iframe and check that there is Epam logo in the left top conner of iframe
-        webDriver.switchTo().frame("jdi-frame-site");
-        Assert.assertTrue(webDriver.findElement(By.id("epam-logo")).isDisplayed());
-
-        //12.Switch to original window back
+        //10. Switch to original window back
         webDriver.switchTo().defaultContent();
 
-        //13.Assert a text of the sub header
-        Assert.assertEquals(webDriver.findElement(By.linkText("JDI GITHUB")).getText(), "JDI GITHUB");
+        //11. Assert that there are 5 items in the Left Section are displayed and they have proper text
+        String [] expectedMenuItems = {"Home", "Contact form", "Service", "Metals & Colors", "Elements packs"};
+        int expectedMenuSize = 5;
 
-        //14.Assert that JDI GITHUB is a link and has a proper URL
-        String expectedUrl = "https://jdi-testing.github.io/jdi-light/index.html";
-        String currentUrl = webDriver.getCurrentUrl();
-        Assert.assertEquals(currentUrl, expectedUrl);
+        assertS.assertTrue(webDriver.findElement(By.name("navigation-sidebar")).isDisplayed());
+        List<WebElement> leftMenuItems = webDriver.findElements(By.xpath("//*[@class='sidebar-menu left']/li"));
 
-        //15.Assert that there is Left Section
-        Assert.assertTrue(webDriver.findElement(By.name("navigation-sidebar")).isDisplayed());
+        assertS.assertEquals(leftMenuItems.size(), expectedMenuSize);
+        for (int i = 0; i < leftMenuItems.size(); i++) {
+            assertS.assertEquals(leftMenuItems.get(i).getText(), expectedMenuItems[i]);
+        }
 
-        //16.Assert that there is Footer
-        Assert.assertTrue(webDriver.findElement(By.xpath("//footer")).isDisplayed());
-
-        //17.Close Browser
+        //12. Close Browser
         webDriver.close();
+        assertS.assertAll();
     }
 }
